570277eaa858446fc6e7d32be3382ee2386b01f5
weidixian <weidixian@126.com>
2025-08-29 21:21:38 +0800

    Add validation for customer existence and contract modifications in ContractInfoHandlerImpl

--- ruoyi-modules/ruoyi-ditalk/src/main/java/org/dromara/handler/impl/ContractInfoHandlerImpl.java
+++ ruoyi-modules/ruoyi-ditalk/src/main/java/org/dromara/handler/impl/ContractInfoHandlerImpl.java
@@ -3,11 +3,15 @@ package org.dromara.handler.impl;
 import com.baomidou.dynamic.datasource.annotation.DSTransactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.dromara.common.core.exception.user.UserException;
 import org.dromara.common.enums.ContractStateEnum;
 import org.dromara.common.satoken.utils.LoginHelper;
 import org.dromara.handler.IContractInfoHandler;
 import org.dromara.module.contract.domain.bo.ContractInfoBo;
+import org.dromara.module.contract.domain.vo.ContractInfoVo;
 import org.dromara.module.contract.service.IContractInfoService;
+import org.dromara.module.customer.domain.vo.CustomerInfoVo;
+import org.dromara.module.customer.service.ICustomerInfoService;
 import org.springframework.stereotype.Service;
 
 /**
@@ -21,6 +25,7 @@ import org.springframework.stereotype.Service;
 public class ContractInfoHandlerImpl implements IContractInfoHandler {
 
     private final IContractInfoService contractInfoService;
+    private final ICustomerInfoService customerInfoService;
 
     /**
      * 新增合同信息，当前用户和部门自动赋值，状态默认为草稿。
@@ -31,6 +36,10 @@ public class ContractInfoHandlerImpl implements IContractInfoHandler {
     @Override
     @DSTransactional
     public Boolean add(ContractInfoBo bo) {
+        CustomerInfoVo customerInfoVo = customerInfoService.queryById(bo.getCustomerId()); // 线索不可以创建合同
+        if (customerInfoVo != null) {
+            throw new UserException("客户不存在，无法创建合同");
+        }
         bo.setAssignedTo(LoginHelper.getUserId());
         bo.setAssignedDept(LoginHelper.getDeptId());
         bo.setState(ContractStateEnum.DRAFT.getCode());
@@ -40,6 +49,16 @@ public class ContractInfoHandlerImpl implements IContractInfoHandler {
     @Override
     @DSTransactional
     public Boolean edit(ContractInfoBo bo) {
+        ContractInfoVo contractInfoVo = contractInfoService.queryById(bo.getId());
+        if (contractInfoVo == null) {
+            throw new UserException("合同不存在");
+        }
+        if (!contractInfoVo.getOpportunityId().equals(bo.getOpportunityId())) {
+            throw new UserException("合同的商机不可以修改");
+        }
+        if (!contractInfoVo.getCustomerId().equals(bo.getCustomerId())) {
+            throw new UserException("合同的客户不可以修改");
+        }
         return contractInfoService.updateByBo(bo);
     }
 }
