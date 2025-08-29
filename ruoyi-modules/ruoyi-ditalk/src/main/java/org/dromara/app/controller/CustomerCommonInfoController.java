408c04b810795013c024a308bfd598e72758ce6b
weidixian <weidixian@126.com>
2025-08-29 21:07:03 +0800

    Fix the prior commit.

--- ruoyi-modules/ruoyi-ditalk/src/main/java/org/dromara/app/controller/CustomerCommonInfoController.java
+++ ruoyi-modules/ruoyi-ditalk/src/main/java/org/dromara/app/controller/CustomerCommonInfoController.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.dromara.common.core.domain.R;
 import org.dromara.common.web.core.BaseController;
+import org.dromara.handler.ICustomerInfoCommonHandler;
 import org.dromara.module.customer.domain.vo.CustomerInfoVo;
-import org.dromara.module.customer.service.ICustomerInfoService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/customer/common/info")
 public class CustomerCommonInfoController extends BaseController {
 
-    private final ICustomerInfoService customerInfoService;
+    private final ICustomerInfoCommonHandler customerInfoCommonHandler;
 
     /**
      * 获取客户信息详细信息
@@ -36,7 +36,7 @@ public class CustomerCommonInfoController extends BaseController {
     @GetMapping("/{id}")
     public R<CustomerInfoVo> getInfo(@NotNull(message = "主键不能为空")
                                      @PathVariable Long id) {
-        return R.ok(customerInfoService.queryById(id));
+        return R.ok(customerInfoCommonHandler.queryAllByIdNoCache(id));
     }
 
 }
